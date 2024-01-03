10459909aa029f8dbbd7751658cfbd8cadc094c6
masudrana2017 <masudranaspi1@gmail.com>
2024-01-03 10:16:55 +0600

    My Second Blog

--- index.php
+++ index.php
@@ -3,11 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style/style.css" rel="stylesheet">
     <title>My First GitHub Website</title>
 </head>
 <body>
     <div>
         <h1>My First Blog</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam quam illum veritatis voluptates quia repellendus quod placeat, cupiditate quidem optio, quae eveniet id. Soluta numquam cum minima facilis velit?</p>
+    </div>
+    <div>
+        <h1>My Second Blog</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam quam illum veritatis voluptates quia repellendus quod placeat, cupiditate quidem optio, quae eveniet id. Soluta numquam cum minima facilis velit?</p>
     </div>
 </body>
