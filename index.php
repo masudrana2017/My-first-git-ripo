926c0b3ab90e35cd5899c804853eb7c9573fe198
masudrana2017 <masudranaspi1@gmail.com>
2024-01-03 10:12:11 +0600

    My First Blog

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My First Git Project</title>
+    <title>My First GitHub Website</title>
 </head>
 <body>
-    
+    <div>
+        <h1>My First Blog</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam quam illum veritatis voluptates quia repellendus quod placeat, cupiditate quidem optio, quae eveniet id. Soluta numquam cum minima facilis velit?</p>
+    </div>
 </body>
 </html>
